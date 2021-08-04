25e8fb53d45152b60206ce31ff3b75f11b9a0fd3
becall01 <elliecall1@gmail.com>
2021-08-04 11:06:43 +0000

    Drives

--- schema_DDL.sql
+++ schema_DDL.sql
@@ -88,3 +88,13 @@ CREATE TABLE PSU (
     wattage int
 );
 
+DROP TABLE IF EXISTS Drives;
+CREATE TABLE Drives (
+    id SERIAL PRIMARY KEY,
+    machine_id INT FOREIGN KEY,
+    tag_id varchar(20) FOREIGN KEY,
+    model varchar(25),
+    size int, 
+    d_type varchar(5),  /* type was a keyword, so changed to d_type(AKA drive type)*/
+    interface varchar(25) /* interface string formating not known, update later*/
+);
