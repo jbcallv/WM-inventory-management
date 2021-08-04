72e6d5cf70db0c5b5613cb55d25fb783050be45d
jbcallv <jbcallv@gmail.com>
2021-08-04 10:00:58 +0000

    schemata changes

--- schema_DDL.sql
+++ schema_DDL.sql
@@ -1,12 +1,6 @@
-DROP TABLE IF EXISTS System;
-CREATE TABLE System (
-    id SERIAL PRIMARY KEY,
-    tag_id varchar(20)
-);
-
 DROP TABLE IF EXISTS Tag;
 CREATE TABLE Tag (
-    id SERIAL PRIMARY KEY,
+    tag_id varchar(8) PRIMARY KEY,
     vendor varchar(20),
     purchase_price int,
     purchase_date DATE NOT NULL DEFAULT CURRENT_DATE
@@ -15,68 +9,33 @@ CREATE TABLE Tag (
 DROP TABLE IF EXISTS Manufacturer;
 CREATE TABLE Manufacturer (
     id SERIAL PRIMARY KEY,
-    manufacturer varchar(20),
+    manufacturer_name varchar(20),
     /* there's a name field in the erd. Is this necessary? This table seems too short for it to be worth keeping */
 );
 
-DROP TABLE IF EXISTS Located;
-CREATE TABLE Located (
+DROP TABLE IF EXISTS Located_at;
+CREATE TABLE Located_at (
     id SERIAL PRIMARY KEY,
     building varchar(25),
     room_number int
 );
 
-DROP TABLE IF EXISTS GPU;
-CREATE TABLE GPU (
+DROP TABLE IF EXISTS Machine;
+CREATE TABLE Machine (
     id SERIAL PRIMARY KEY,
-    /* is this necessary since we have a location table that can be accessed using serial field? located varchar(255) blah blah*/
-    tag_id varchar(20), 
-    vram INT NOT NULL,
+    tag_id varchar(20)
 );
 
-DROP TABLE IF EXISTS Memory;
-CREATE TABLE Memory (
+DROP TABLE IF EXISTS Model;
+CREATE TABLE Model (
     id SERIAL PRIMARY KEY,
-    size int,
-    type_generation varchar(10),
-    type_speed int,
-    type_form_factor varchar(4)
+    model_name varchar(25)
 );
 
-DROP TABLE IF EXISTS CPU;
-CREATE TABLE CPU (
+DROP TABLE IF EXISTS Component;
+CREATE TABLE Component (
     id SERIAL PRIMARY KEY,
-    threads int,
-    cores, int
+    machine_id INT FOREIGN KEY,
+    tag_id varchar(20) FOREIGN KEY
 );
 
-DROP TABLE IF EXISTS Motherboard;
-CREATE TABLE Motherboard (
-    id SERIAL PRIMARY KEY
-);
-
-DROP TABLE IF EXISTS NC;
-CREATE TABLE NC (
-    id SERIAL PRIMARY KEY
-);
-
-DROP TABLE IF EXISTS PSU;
-CREATE TABLE PSU (
-    id SERIAL PRIMARY KEY,
-    wattage int
-);
-
-DROP TABLE IF EXISTS Drives;
-CREATE TABLE Drives (
-    id SERIAL PRIMARY KEY,
-    size int,
-    drive_type varchar(4),
-    interface varchar(12)
-);
-
-DROP TABLE IF EXISTS Display
-CREATE TABLE Display (
-    id SERIAL PRIMARY KEY,
-    size int,
-    resolution varchar(10)
-);
